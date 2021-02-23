5e13daa187041f5b21e8058e3bf1c718a820b2be
Michael Evangelos Diamantis <michaeldiamantis17@outlook.com>
2021-02-24 00:39:10 +0200

    Second Task completed.

--- exer6.cpp
+++ exer6.cpp
@@ -3,18 +3,32 @@
 
 void PrintTransaction()
 {
-	Sales_item book;
+	Sales_item item;
 
 	for (int i = 0;i <= 2;++i)
 	{
-		std::cin >> book;
-		std::cout << book << std::endl;
+		std::cin >> item;
+		std::cout << item << std::endl;
+	}
+}
+
+void PrintSummedTransaction()
+{
+	Sales_item item1, item2;
+
+	for (int i = 0; i < 2; i++)
+	{
+		std::cin >> item1 >> item2;
+		std::cout << item1 + item2 << std::endl;
 	}
 }
 
 int main()
 {
 	PrintTransaction(); //Task 1.20
+	system("pause");
+
+	PrintSummedTransaction();
 	system("pause");
 
 	return 0;
